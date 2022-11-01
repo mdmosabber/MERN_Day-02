c00a856f19915d9ff40be06c9b9ceb99d59c747e
= <=>
2022-11-01 06:09:13 +0600

    update class-09

---
+++ Day-02/class-09/parseInt.js
@@ -0,0 +1,6 @@
+var number1 = '10';
+var number2 = '20';
+
+console.log('With Number Method: ', Number(number1) + Number(number2));
+console.log('With parseInt Method: ', Number(number1) + Number(number2)); //Comma separator
+console.log('With parseInt Method: '+ ( parseInt(number1) + parseInt(number2) ) ); // Plus(+) concatenation
