d059ba77b0f75d5c3ed4c43faf4d735eaa833354
= <=>
2022-11-01 06:21:04 +0600

    upload float number

--- Day-02/class-09/parseInt.js
+++ Day-02/class-09/parseInt.js
@@ -1,6 +1,7 @@
 var number1 = '10';
-var number2 = '20';
+var number2 = '20.001';
 
 console.log('With Number Method: ', Number(number1) + Number(number2));
 console.log('With parseInt Method: ', Number(number1) + Number(number2)); //Comma separator
 console.log('With parseInt Method: '+ ( parseInt(number1) + parseInt(number2) ) ); // Plus(+) concatenation
+console.log('With parseInt Method: '+ ( parseFloat(number1) + parseFloat(number2) ) ); // Plus(+) concatenation with parseFloat
